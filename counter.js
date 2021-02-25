32c7b1372d5f4c504f390b08a512bb945a290f90
Luis yChun <yongchun903@gmail.com>
2021-02-25 22:42:44 +0800

    fix: update counter timer

--- counter.js
+++ counter.js
@@ -1,28 +1,30 @@
-const countdown = document.querySelector('.ls-counter');
+const countdown = document.querySelector('.ls-counter')
 
-const lauchDate = new Date('March 1, 2021 12:00:00').getTime();
+const lauchDate = new Date('May 1, 2021 12:00:00').getTime()
 
-const int = setInterval(function(){
-    const current = new Date().getTime();
+const int = setInterval(function () {
+  const current = new Date().getTime()
 
-    const distance = lauchDate - current;
+  const distance = lauchDate - current
 
-    const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    const secs = Math.floor((distance % (1000 * 60)) / 1000);
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24))
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  )
+  const mins = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+  const secs = Math.floor((distance % (1000 * 60)) / 1000)
 
-    countdown.innerHTML = `
+  countdown.innerHTML = `
         <div>${days}<span>Days</span></div>
         <div>${hours}<span>Hours</span></div>
         <div>${mins}<span>Mins</span></div>
         <div>${secs}<span>Secs</span></div>
-    `;
+    `
 
-    if(distance < 0) {
-        clearInterval(int);
+  if (distance < 0) {
+    clearInterval(int)
 
-        countdown.style.color='#17a2b8';
-        countdown.innerHTML = 'Launched';
-    }
-}, 1000);
+    countdown.style.color = '#17a2b8'
+    countdown.innerHTML = 'Launched'
+  }
+}, 1000)
